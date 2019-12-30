8273043067a08c26a6023e039a198c62ac7adf53
Bilal <mk.bilal92@gmail.com>
2019-12-31 02:44:28 +0500

    Services

---
+++ src/app/Services/layout.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import{Observable} from'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LayoutService {
+
+  constructor(private http:HttpClient) {  }
+
+
+public getAllcategories():Observable<any>{
+
+return this.http.get("http://localhost:3000/categories");
+
+}
+}
